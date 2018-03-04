43a1f1198d5834c12bc829be09d75aa927877199
cagricakir <cagricakir995@gmail.com>
2018-03-04 12:25:58 +0300

    Button is created and configured to passing the other class.

---
+++ app/src/main/java/codedbycagri/intentexample/MainActivity.java
@@ -0,0 +1,26 @@
+package codedbycagri.intentexample;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, intentClass.class);
+                MainActivity.this.startActivity(intent);
+                MainActivity.this.finish();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/codedbycagri/intentexample/intentClass.java
@@ -0,0 +1,14 @@
+package codedbycagri.intentexample;
+
+import android.os.Bundle;
+
+/**
+ * Created by vsski on 4.03.2018.
+ */
+
+public class intentClass extends MainActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.intent_example);
+
+}}
